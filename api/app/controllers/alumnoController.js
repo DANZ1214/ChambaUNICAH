93172b8a1f920e4be7c2d8edec4db00e9d353a1b
WilsonFerrer <ferrerawilson76@gmail.com>
2025-03-15 00:07:44 -0600

    funcion update Alumno

--- api/app/controllers/alumnoController.js
+++ api/app/controllers/alumnoController.js
@@ -49,12 +49,37 @@ async function deleteAlumno(req, res) {
     });
 }
 
+async function updateAlumno(req, res) {
+    const { alumnoId, nombre, facultadId } = req.body; // Recibimos los datos
+
+    if (!alumnoId || !nombre || !facultadId) {
+        return res.status(400).send({ message: "Todos los campos son obligatorios" });
+    }
+
+    alumno.update(
+        { nombre, facultadId }, // Campos a actualizar
+        { where: { alumnoId } } // Condición: actualizar solo el alumno con este ID
+    )
+    .then(([rowsUpdated]) => { // Sequelize devuelve un array, el primer valor es el número de filas afectadas
+        if (rowsUpdated > 0) {
+            res.status(200).send({ message: "Alumno actualizado exitosamente" });
+        } else {
+            res.status(404).send({ message: "Alumno no encontrado" });
+        }
+    })
+    .catch(error => {
+        res.status(500).send({ message: error.message || "Error al actualizar alumno" });
+    });
+}
+
+
 
 
 
 module.exports = {
     getAlumno,
     insertAlumno,
-    deleteAlumno
+    deleteAlumno,
+    updateAlumno
 }
 
--- api/app/routes/alumnoRoutes.js
+++ api/app/routes/alumnoRoutes.js
@@ -7,5 +7,7 @@ const apiRoutes = express.Router();
 apiRoutes.get('/getAlumno',async(req, res) => await alumnoController.getAlumno(req, res));
 apiRoutes.post('/insertAlumno',async(req, res) => await alumnoController.insertAlumno(req, res));
 apiRoutes.delete('/deleteAlumno',async(req, res) => await alumnoController.deleteAlumno(req, res));
+apiRoutes.put('/updateAlumno', async (req, res) => await alumnoController.updateAlumno(req, res));
+
 
 module.exports = apiRoutes;
