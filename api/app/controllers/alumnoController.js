6d8f87e2710368761b579409d74c9fa951d332ad
WilsonFerrer <ferrerawilson76@gmail.com>
2025-03-14 23:04:58 -0600

    funcion delete en alumno controller

--- api/app/controllers/alumnoController.js
+++ api/app/controllers/alumnoController.js
@@ -27,10 +27,30 @@ async function insertAlumno(req, res) {
     });
 }
 
+async function deleteAlumno(req, res) {
+    const { alumnoId } = req.params; // Obtener el ID del alumno desde los parámetros de la URL
+
+    alumno.destroy({
+        where: { alumnoId } // Condición para eliminar el registro
+    })
+    .then(deleted => {
+        if (deleted) {
+            res.status(200).send({ message: "Alumno eliminado exitosamente" });
+        } else {
+            res.status(404).send({ message: "Alumno no encontrado" });
+        }
+    })
+    .catch(error => {
+        res.status(500).send({ message: error.message || "Error al eliminar alumno" });
+    });
+} 
+
+
 
 
 module.exports = {
     getAlumno,
-    insertAlumno
+    insertAlumno,
+    deleteAlumno
 }
 
